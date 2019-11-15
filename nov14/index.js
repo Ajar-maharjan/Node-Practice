c765e43e72a3b19485744213181daa1caba1ae4d
Ajar-maharjan <pleiades.ajar@gmail.com>
2019-11-15 08:43:59 +0545

    day2

--- nov14/index.js
+++ nov14/index.js
@@ -1,12 +1,45 @@
 //common js
-var http = require('http');
+//var http = require('http');
 //console.log(http);
+const express = require('express');
+const app = express();
 
+//authenticating middleware
+app.get('/auth', 
+function(req,res,next){
+    console.log('in first middleware do something');
+    next();
+},
+function(req,res,next){
+    console.log('in second middleware do something');
+    res.status(200);
+    res.set({
+        'Content-Type' : 'plain'
+    });
+    res.send('Nurse Akali');
+})
+
+
+//route
+app.get('/',function(req,res){
+//handle
+console.log('True damage akali');
+//json format
+var x = {name: "true damage",test:'akali'};
+//set
+res.status(200);
+res.set({'Content-Type':'application/json'});
+res.json(x);
+//res.send('KDA akali');
+})
+app.listen(3091);
+
+/*
 function callbackregister(req,res){
     //handle
-    //console.log('akali')
+    console.log('KDA akali');
     res.statusCode = 200;
-    res.end('this is homepage');
+    res.end('KDA akali');
 }
 //handle function
 
@@ -15,5 +48,5 @@ var application = http.createServer(callbackregister);
 //handle request
 
 console.log('server is running in 3090 port');
-application.listen(3090);
+application.listen(3090); */
 
