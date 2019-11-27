d4e2b8a44ac255986073590c1a88ac4d859921a6
Ajar-maharjan <pleiades.ajar@gmail.com>
2019-11-27 10:44:28 +0545

    body-parser

---
+++ nov27/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+// app.get('/hospitallist',function(req,res,next){
+//     console.log(req.query);
+//     res.send('req recieved');
+// })
+// app.listen(3001);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.post('/registration',function(req,res,next){
+    console.log(req.query);
+    res.send ('req recieved');
+})
+app.listen(3002);
