ca0cf3f104e26da1e237aa2212384739f4b4fc5a
Ajar-maharjan <pleiades.ajar@gmail.com>
2019-11-29 08:23:10 +0545

    middleware

--- nov27/index.js
+++ nov27/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('database', 'root', 'pleiades', {
+  host: 'localhost',
+  dialect: 'mysql' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+});
+
 // app.get('/hospitallist',function(req,res,next){
 //     console.log(req.query);
 //     res.send('req recieved');
