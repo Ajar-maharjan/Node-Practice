1a86d1c27cbcf567b0759ff71b0dc76cc21f4c42
Ajar-maharjan <pleiades.ajar@gmail.com>
2020-01-03 08:36:40 +0545

    jsx

--- react25/src/index.js
+++ react25/src/index.js
@@ -5,7 +5,7 @@
 import React from 'react'; //require react = require('react');
 import ReactDOM from 'react-dom';
 
-//react renderer
+//* react renderer
 ReactDOM.render(
     //1st param wat to render
     //the element to be created is html element
@@ -24,10 +24,41 @@ let test = (a, b) => {
 
 };
 
-//normal renderer
+//* normal renderer
 const renderer = () => {
     document.getElementById('normal').innerHTML =
         `<p> this is list <input> </input></p> <p> ${new Date().toLocaleTimeString()} </P>`;
 
 }
 setInterval(renderer, 1000);
+
+
+//* jsx render
+// button h1 div
+const lecture = 'of react jsx';
+
+const element = <h1> This is testing {
+    lecture
+} </h1>;
+ReactDOM.render(element, document.getElementById("jsx"));
+
+// component creation
+
+// two types - function based and class based
+//function so that it can be reused
+function Button(porps) {
+    return <button type = "submit" > {
+        porps.label
+    } </button> 
+    // this is jsx
+}
+
+//? this also works, but cannot be reused vvvv
+// ReactDOM.render(<button type = "submit"></button>,document.getElementById("jsx"));
+//! Button function is called and button is actually component here
+
+// this is  to save
+ReactDOM.render( < Button label = "save" / > , document.getElementById("jsx"));
+
+// this is to cancel
+ReactDOM.render( < Button label = "cancel" / > , document.getElementById("jsxx"));
