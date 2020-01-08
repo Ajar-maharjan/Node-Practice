4443f694b6f3ea5852124f14134ec1961a5c30cb
Ajar-maharjan <pleiades.ajar@gmail.com>
2020-01-08 10:15:49 +0545

    state props

--- react25/src/index.js
+++ react25/src/index.js
@@ -20,9 +20,9 @@ ReactDOM.render(
     //where to render
     document.getElementById('react')
 )
-let test = (a, b) => {
+// let test = (a, b) => {
 
-};
+// };
 
 //* normal renderer
 const renderer = () => {
@@ -33,7 +33,7 @@ const renderer = () => {
 setInterval(renderer, 1000);
 
 
-//* jsx render
+//* jsx render // better
 // button h1 div
 const lecture = 'of react jsx';
 
@@ -43,22 +43,81 @@ const element = <h1> This is testing {
 ReactDOM.render(element, document.getElementById("jsx"));
 
 // component creation
-
 // two types - function based and class based
 //function so that it can be reused
-function Button(porps) {
+
+/*
+? components of reacts
+* props
+* state
+*/
+
+/* lifecycle
+1. mount
+2. update
+3. unmount
+*/
+
+//* props 
+function Button(props) {
     return <button type = "submit" > {
-        porps.label
+        props.label
     } </button> 
     // this is jsx
 }
+
+//*  state props base class
+class H1 extends React.Component {
+    constructor(){
+    super() // initialize parent class of constructor ie: react components
+    this.state = {
+        testVar : 10,
+        varTest : "this is test",
+        label : "Save"
+    }}
+    // TODO: own logic 
+    componentWillMount(){ // Before components mounts/render
+
+    }
+    componentDidMount(){ // after components mounts/render
+        this.setState({label:'Cancel'});
+    }
+    shouldComponentUpdate(nextProps,nextState){ //whether the componenets should be updated
+    // return boolean value, default is true
+        console.log(this.state);
+        console.log(nextState);
+        return this.state.label !== nextState.label;
+        // return true;
+    }
+    componentWillUpdate(nextProps,nextState){
+    // just before component updates
+    console.log(nextProps);
+    console.log(nextState);
+    }
+    componentDidUpdate(){
+    // after component udpates
+    
+    }
+    componentWillUnmount(){
+    // before a component is unmounted 
+    
+    }
+    render(){
+        return <h1>{this.state.label}</h1>
+    }
+}
+
+
 
 //? this also works, but cannot be reused vvvv
 // ReactDOM.render(<button type = "submit"></button>,document.getElementById("jsx"));
 //! Button function is called and button is actually component here
 
 // this is  to save
-ReactDOM.render( < Button label = "save" / > , document.getElementById("jsx"));
+ReactDOM.render( <Button label = "save"/> , document.getElementById("jsx"));
 
 // this is to cancel
-ReactDOM.render( < Button label = "cancel" / > , document.getElementById("jsxx"));
+ReactDOM.render( <Button label = "cancel"/> , document.getElementById("jsxx"));
+
+//render state test
+ReactDOM.render( <H1 label = "save"/>,document.getElementById("state"));
